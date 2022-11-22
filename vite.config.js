cacf2020f186fea753a02c44a49393f516b60d8f
Lastor-Chen <lastorchen@onead.com.tw>
2022-11-22 14:18:12 +0800

    [Add] deploy 設定

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command }) => {
+  const isDev = command === 'serve'
+  return {
+    plugins: [vue()],
+    base: isDev ? '/' : '/frontend-deploy-demo/'
+  }
 })
